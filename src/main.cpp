3d2cd9dda8d76ffd16a9d70fb6acc1201ed74838
Mauro de Lyon <maurodelyon@gmail.com>
2017-12-19 16:19:04 +0100

    removed code and added comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
-void setup() {
-    // put your setup code here, to run once:
+SoftwareSerial RS485Serial(6, 5);
+
+void setup()
+{
+    // Use serial to communicate with the main application
+    Serial.begin(9600);
+
+    // Use software serial to communicate with the RS485 bus
+    RS485Serial.begin(9600);
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void loop()
+{
+    // Communication: RS485 bus --> Main application
+    if (RS485Serial.available())
+    {
+        while (RS485Serial.available())
+        {
+            Serial.write(RS485Serial.read());
+        }        
+    }
+}
+
+void serialEvent()
+{
+    // Communication: Main application --> RS485 bus
+    if (Serial.available())
+    {
+        while (Serial.available())
+        {
+            RS485Serial.write(Serial.read());
+        }
+    }
 }
